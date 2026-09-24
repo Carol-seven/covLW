a017fc648fab33e9d51bb8ca4011475808473350
Carol-seven <542046605@qq.com>
2026-09-23 22:10:54 -0400

    mv ex to inst/example

--- R/covest.R
+++ R/covest.R
@@ -57,19 +57,8 @@
 #' @references
 #' \insertAllCited{}
 #'
-#' @examples
-#' set.seed(123)
-#' X <- matrix(rnorm(100), nrow = 20, ncol = 5)
-#'
-#' # Center X internally and use an effective sample size of N - 1
-#' Sigma_hat <- covest(X)
-#'
-#' # X has already been centered as one class
-#' X_centered <- scale(X, center = TRUE, scale = FALSE)
-#' Sigma_hat_centered <- covest(X_centered, k = 1)
-#'
-#' # Population mean is assumed to be known and equal to zero
-#' Sigma_hat_zeromean <- covest(X, k = 0)
+#' @example
+#' inst/example/ex-covest.R
 #'
 #' @importFrom Rdpack reprompt
 #'
